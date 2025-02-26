91c87b1c818eeef7d93fe250c9176779fed451a0
fredrik <freedrik92@hotmail.com>
2025-02-26 21:27:32 +0100

    wrong file name

--- TestRazorApp/Program.cs
+++ TestRazorApp/Program.cs
@@ -10,8 +10,6 @@ builder.Services.AddSingleton<ProductService>();
 
 var app = builder.Build();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Urls.Add($"http://0.0.0.0:{port}");
 
 
 if (!app.Environment.IsDevelopment())//migth comment out during development
